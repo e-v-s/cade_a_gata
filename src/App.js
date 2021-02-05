c6415e17729fe0fe73ae56fddbe8794a16c07927
Évora evs Leite <evoraleite@gmail.com>
2021-02-05 11:16:46 -0500

    deploy again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/header';
 import Body from './components/body';
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 function App() {
   // const [menuType, setMenuType] = useState('');
